274b4289a6d1d7b1edfd6b9d368cd204a4072095
vadymdidyk <vadymdidyk.work@protonmail.com>
2026-02-26 08:25:46 +0900

    Added basic ssl, exposed local dev server to network

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    basicSsl()
   ],
   test: {
     globals: true,
